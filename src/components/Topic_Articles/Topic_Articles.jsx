ff1bd0c5f0e32ce80b17238cc0479ac1ceaf32c7
TianYu-00 <imniyeye@gmail.com>
2024-06-17 17:43:33 +0100

    add Click redirect link and nav

--- src/components/Topic_Articles/Topic_Articles.jsx
+++ src/components/Topic_Articles/Topic_Articles.jsx
@@ -11,6 +11,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import { Link } from "react-router-dom";
 
 export default function Topic_Articles({ articles, setAllArticles }) {
   let [isArticleLoading, setIsArticleLoading] = useState(false);
@@ -66,28 +67,30 @@ export default function Topic_Articles({ articles, setAllArticles }) {
             let articleDate = new Date(article.created_at).toLocaleDateString("en-UK");
             return (
               <React.Fragment key={index}>
-                <Item sx={{ maxWidth: "750px" }}>
-                  <Grid container sx={{ border: "1px solid blue" }}>
-                    <Grid xs={12} sm={6} sx={{ border: "1px solid pink" }}>
-                      <img
-                        src={article.article_img_url}
-                        alt={article.title}
-                        style={{ width: "100%", objectFit: "cover" }}
-                        loading="lazy"
-                      />
+                <Link to={`/article/${article.article_id}`}>
+                  <Item sx={{ maxWidth: "750px" }}>
+                    <Grid container sx={{ border: "1px solid blue" }}>
+                      <Grid xs={12} sm={6} sx={{ border: "1px solid pink" }}>
+                        <img
+                          src={article.article_img_url}
+                          alt={article.title}
+                          style={{ width: "100%", objectFit: "cover" }}
+                          loading="lazy"
+                        />
+                      </Grid>
+                      <Grid xs={12} sm={6} sx={{ border: "1px solid pink" }}>
+                        <h4>{article.title}</h4>
+                        <p>{article.author}</p>
+                        <p>Topic: {article.topic}</p>
+                      </Grid>
                     </Grid>
-                    <Grid xs={12} sm={6} sx={{ border: "1px solid pink" }}>
-                      <h4>{article.title}</h4>
-                      <p>{article.author}</p>
-                      <p>Topic: {article.topic}</p>
+                    <Grid>
+                      <p style={{ fontSize: "0.75rem", textWrap: "nowrap" }}>
+                        Votes: {article.votes} | Comments: {article.comment_count} | Created At: {articleDate}
+                      </p>
                     </Grid>
-                  </Grid>
-                  <Grid>
-                    <p style={{ fontSize: "0.75rem", textWrap: "nowrap" }}>
-                      Votes: {article.votes} | Comments: {article.comment_count} | Created At: {articleDate}
-                    </p>
-                  </Grid>
-                </Item>
+                  </Item>
+                </Link>
               </React.Fragment>
             );
           })}
