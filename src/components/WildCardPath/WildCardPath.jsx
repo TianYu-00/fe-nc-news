0f2b2171133b1b8bb974d53c1de33eaa5230d669
TianYu-00 <imniyeye@gmail.com>
2024-06-21 17:47:22 +0100

    add more styling to 404 page

--- src/components/WildCardPath/WildCardPath.jsx
+++ src/components/WildCardPath/WildCardPath.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 export default function WildCardPath() {
   const navigate = useNavigate();
   return (
     <>
-      <h2>404</h2>
-      <p>Page Not Found</p>
-      <Button
-        variant="contained"
-        onClick={() => {
-          navigate("/home");
+      <Box
+        sx={{
+          width: "100%",
+          height: "83vh",
+          backgroundColor: "#1A2027",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          alignItems: "center",
+          padding: "2rem",
         }}
       >
-        GO TO HOME
-      </Button>
+        <h2>404</h2>
+        <p>Page Not Found</p>
+        <Button
+          variant="contained"
+          onClick={() => {
+            navigate("/home");
+          }}
+          sx={{ marginTop: "25px", width: "200px" }}
+        >
+          GO TO HOME
+        </Button>
+      </Box>
     </>
   );
 }
